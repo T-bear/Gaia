f37b9d8124b0c6c18a23edd6befbf9b166477b98
torbjorn.holgersson87@gmail.com <torbjorn.holgersson87@gmail.com>
2018-05-24 11:29:46 +0200

    added city route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import  Flask, render_template, jsonify, redirect
 from pymongo import MongoClient
 from bson.json_util import dumps
+from httperrors import  UsageError
 
 app = Flask(__name__)
 
