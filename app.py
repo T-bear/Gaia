450dfd83f94bbad216b16ef1d498775d4b88e95c
torbjorn.holgersson87@gmail.com <torbjorn.holgersson87@gmail.com>
2018-05-28 11:15:53 +0200

    added update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import  Flask, render_template, jsonify, redirect
 from pymongo import MongoClient
 from bson.json_util import dumps
-from bson.objectid imprt ObjectId
+from bson.objectid import ObjectId
 import json
 
 app = Flask(__name__)
